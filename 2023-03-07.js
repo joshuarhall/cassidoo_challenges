fd5f42855dcb0cb854ffe2fa3c472b8086cc926e
Josh Hall <67877014+joshuarhall@users.noreply.github.com>
2023-03-11 13:19:22 -0800

    Update 2023-03-07.js

--- 2023-03-07.js
+++ 2023-03-07.js
@@ -11,6 +11,14 @@ function scramble(str) {
         if (el.length > 3) {
             // scramble all  letters for practice
             el = el.split("") // el is now in an array by letters
+            // console.log(el)
+            for (let i = el.length - 1; i > 1; i--) {
+                // pick a random index from 0 to i inclusive
+                const j = Math.floor(Math.random() * (i + 1)); // at random index
+                // Swap arr[i] with the element
+                [el[i], el[j]] = [el[j], el[i]];
+            }
+
             // see this for randomizing https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html 
             el = el.join("")
 
@@ -22,3 +30,19 @@ function scramble(str) {
 
 console.log(scramble("this is a test of the function called scramble"))
 
+
+
+// function shuffleArray(arr) {
+//     // Start from the last element and swap
+//     // one by one. We don't need to run for
+//     // the first element that's why i > 0
+//     for (let i = newArr.length - 2; i > 1; i--) {
+//       // pick a random index from 0 to i inclusive
+//       const j = Math.floor(Math.random() * (i + 1)); // at random index
+//       // Swap arr[i] with the element
+//       [arr[i], arr[j]] = [arr[j], arr[i]];
+//     }
+//     console.log(arr);
+//   }
+//   let arr = [1, 2, 3, 4, 5];
+//   shuffleArray(arr);
